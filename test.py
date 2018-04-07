b105a344953e3c87893a7afd343ea6e2e4dcdfa4
whsasf <whsasf@126.com>
2018-04-07 17:56:09 +0800

    new

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 
+import builtwith as bw
+import urllib3 as ul3
+
+
+
+http = ul3.PoolManager()
+try:
+    r = http.request('GET', 'https://www.jd.com',timeout=4.0)
+except ul3.exceptions.NewConnectionError:
+    print('Connection failed.')
+
+print (r.status)
+#print (r.data.decode('utf-8'))
+with open('baidu.html','a') as haha:
+    haha.write(r.data.decode('utf-8'))
